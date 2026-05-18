93adb9d1151d9d11d391d0ad22b83cbb51c53d9b
Mudasir Mohammad Bhat <139787715+mudasirr13@users.noreply.github.com>
2026-05-18 14:46:48 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,10 @@
+-- Create Database "DataWareHouse"
+
+create database  DataWareHouse;
+use  DataWareHouse;
+
+-- create schemas
+ 
+create schema bronze;
+create schema silver;
+create schema gold;
